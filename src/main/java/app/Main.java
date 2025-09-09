8c60e34ee66f05fad0c774d3a84c3e2b06862294
gabrielcorreia1997 <gabriel_camargo_correia@hotmail.com>
2025-09-09 00:41:48 +0000

    Aula 08/09/2025 - Sem Herança

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,15 +1,30 @@
 package app;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Main {
     public static void main(String[] args){
-        Tarefa[] tarefas = new Tarefa[5];
-        Compromisso[] compromissos = new Compromisso[5];
 
-        tarefas[0] = new Tarefa(0, "Teste", false);
-        tarefas[4] = new Tarefa(4, "Novo Teste", false);
 
-        compromissos[2] = new Compromisso(0, "Compromisso", "Hoje", "Amanhã");
+        //  Código comentado aula 01-09
+        //  Tarefa[] tarefas = new Tarefa[5];
+        //  Compromisso[] compromissos = new Compromisso[5];
 
-        System.out.println(tarefas[4].getDescricao());
+        //  tarefas[0] = new Tarefa(0, "Teste", false);
+        //  tarefas[4] = new Tarefa(4, "Novo Teste", false);
+
+        //  compromissos[2] = new Compromisso(0, "Compromisso", "Hoje", "Amanhã");
+
+        //  System.out.println(tarefas[4].getDescricao());
+    
+        PessoaFisica pf = new PessoaFisica("Marco", "Rua sem nome, 0", LocalDate.of(1980, Month.MARCH, 29), "12345678");
+    
+        System.out.println(pf.exibirInformacoes());
+    
+        PessoaJuridica pj = new PessoaJuridica("FATEC", "Av. Shunji Nishimura", "FATEC Pompeia", "1234564646513215");
+
+        System.out.println(pj.exibirInformacoes());
+
     }
 }
---
+++ src/main/java/app/PessoaFisica.java
@@ -0,0 +1,71 @@
+package app;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class PessoaFisica{
+	private String nome;
+	private String endereco;
+	private LocalDate dataCriacao;
+    private LocalDate dataNascimento;
+    private String cpf;
+
+    public PessoaFisica (String nome, String endereco, LocalDate dataNascimento, String cpf){
+        this.setNome(nome);
+        this.setDataCriacao(LocalDate.now());
+        this.setEndereco(endereco);
+        this.setDataNascimento(dataNascimento);
+        this.setCpf(cpf);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao){
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf ;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String exibirInformacoes() {
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return "---Informações da Pessoa Física---\n" +
+                "Nome: " + this.getNome() + "\n" + 
+                "Data de Nascimento: " + this.getDataNascimento().format(formatador) + "\n" +
+                "Endereço: " + this.getEndereco() + "\n" + 
+                "CPF: " + this.getCpf() + "\n" + 
+                "---------------------------------------";
+    }
+
+}
